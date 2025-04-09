523039520426832602de150633cda99431d8de64
BlejanStefan <dble440@g.educaand.es>
2025-04-09 15:42:00 +0200

    prueba rebase 3

--- main/src/main/java/calculadora/Calculadora.java
+++ main/src/main/java/calculadora/Calculadora.java
@@ -41,7 +41,7 @@ public class Calculadora {
     private int multiplicacion(int num1, int num2) {
         return num1 * num2;
     }
-    private double division(int num1, int num2) throws  ArithmeticException {
+    private double division(int num1, int num2) {
         if (num2 == 0) {
             throw new ArithmeticException("No se puede dividir entre cero");
         }
